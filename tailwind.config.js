72ba3093edfbd4f63a61f5968a91bd7d316ad0e3
Ishan <Braviaspan2002@gmail.com>
2023-06-29 01:25:26 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        fuchsia : colors.fuchsia,
+      }
+    },
+  },
+  plugins: [],
+}
+
